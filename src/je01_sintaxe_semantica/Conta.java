a0f1febcc7e8420582e5e052dc3e849e1dd7eb5e
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-14 16:43:49 -0300

    feat: indentation

--- src/je01_sintaxe_semantica/Conta.java
+++ src/je01_sintaxe_semantica/Conta.java
@@ -2,11 +2,11 @@ package je01_sintaxe_semantica;
 
 public class Conta {
   //variavel da classe conta
-  double saldo=10.0
+  double saldo=10.0;
 
     public void sacar(Double valor) {
     //variavel do método
-    double novoSaldo = saldo -valor
+    double novoSaldo = saldo -valor;
     }
     public void imprimirSaldo() {
       //disponível em toda classe
