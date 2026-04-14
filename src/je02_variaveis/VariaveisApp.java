d2e731865af694360067f9c84f126f80c579333d
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-14 16:31:52 -0300

    feat: variables

---
+++ src/je02_variaveis/VariaveisApp.java
@@ -0,0 +1,11 @@
+package je02_variaveis;
+
+public class VariaveisApp {
+  public static void main(String[] args) {
+    // Declaração válida de variáveis
+    int numero$um = 1;
+    int numero1 = 1;
+    int numeroUm = 1; //ou numeroUm
+    int longo = 1;
+  }
+}
